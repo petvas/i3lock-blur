fc3390388b296e4dc7c40553a9e538528f796fca
Artymort <m.berta@laposte.net>
2016-08-19 20:57:37 +0200

    Remove the tmp screenshot

--- lock.sh
+++ lock.sh
@@ -5,3 +5,4 @@ RES=1366x768
  
 ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG
 i3lock -i $TMPBG
+rm $TMPBG
