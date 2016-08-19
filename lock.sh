472e53405394469af411bebc07a2b1c72ce1ba78
Artymort <m.berta@laposte.net>
2016-08-19 21:02:06 +0200

    Dirty reverse because I modified the wrong line

--- lock.sh
+++ lock.sh
@@ -3,6 +3,6 @@ TMPBG=/tmp/screen.png
 LOCK=$HOME/lock.png
 RES=$(xrandr | grep '*' | sed -E "s/[^0-9]*([0-9]+)x([0-9]+).*/\1*\2/")
  
-ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG
-i3lock -i $TMPBG -loglevel quiet
+ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
+i3lock -i $TMPBG
 rm $TMPBG
