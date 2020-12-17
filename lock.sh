861232426d97cd05a947c8a44eb8a56fa1f5ca11
Baptiste Dauphin <baptistedauphin76@gmail.com>
2020-12-17 13:58:20 +0100

    add troll to lock screen + set blur ratio to 5:1

--- lock.sh
+++ lock.sh
@@ -2,8 +2,10 @@
 TMPBG=/tmp/screen.png
 DIR=$(dirname "$(realpath $0)")
 LOCK=$DIR/lock.png
+TROLL=$DIR/troll.png
 RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
-BLUR_RATIO='7:1'
-ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=$BLUR_RATIO,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
-i3lock -i $TMPBG
+BLUR_RATIO='5:1'
+
+ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $TROLL -filter_complex "boxblur=$BLUR_RATIO,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
+SDL_VIDEO_FULLSCREEN_HEAD=1 i3lock -i $TMPBG
 rm $TMPBG
