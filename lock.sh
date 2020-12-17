600a5f74b9aa1fd23e3032c11168d06792716fd8
Baptiste Dauphin <baptistedauphin76@gmail.com>
2020-12-17 22:25:26 +0100

    add show failed attempts

--- lock.sh
+++ lock.sh
@@ -7,5 +7,5 @@ RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x
 BLUR_RATIO='5:1'
 
 ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $TROLL -filter_complex "boxblur=$BLUR_RATIO,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
-SDL_VIDEO_FULLSCREEN_HEAD=1 i3lock -i $TMPBG
+SDL_VIDEO_FULLSCREEN_HEAD=1 i3lock --image=$TMPBG --show-failed-attempts
 rm $TMPBG
